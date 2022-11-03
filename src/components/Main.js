800d942a0841ff7282cd569bb26ed8998844a970
sudoaptrana <aryan.rana2021@vitbhopal.ac.in>
2022-11-04 00:30:12 +0530

    update colours using className prop

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles({
-  outlined:{
-    border: "1px solid  #1d72e7"
-  }
-})
-
+import '../style.css';
 
 export default function Main() {
-
-  const classes = useStyles();
-
   return (
     <Box
       component="form"
@@ -23,7 +13,8 @@ export default function Main() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" className={classes.outlined} />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" className="outline" />
+
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
